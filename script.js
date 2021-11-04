30ce190c1f99d90dabfa5c75b042bee8977099c9
Edgar Ernesto Campo Jaime <68762462+edgarcj@users.noreply.github.com>
2021-11-03 21:15:42 -0600

    Create service worker
    Add service worker for instalation

---
+++ script.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "v1_cache_electrico",
+  urlsToCache = ["./", "./style.css", "./script.js","./assets/main_image.jpg", "./assets/medidor.png", "./assets/medidor_64.png", "./assets/main_image.jpg", "./assets/politicas.html","./assets/Terminos.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log("fallo el registro", err))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        });
+      })
+
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+
+      return fetch(e.request);
+    })
+  );
+});
